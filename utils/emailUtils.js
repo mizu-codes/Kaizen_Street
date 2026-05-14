bb32835d02569fb0b36acbbaadc4a797bfd29584
mizu-codes <mizhanmichu9966@gmail.com>
2026-05-14 12:48:58 +0530

    changes made in verification email

--- utils/emailUtils.js
+++ utils/emailUtils.js
@@ -9,9 +9,6 @@ const sendVerificationEmail = async (email, otp) => {
 
         const transporter = nodemailer.createTransport({
             service: 'gmail',
-            port: 587,
-            secure: false,
-            requireTLS: true,
             auth: {
                 user: process.env.NODEMAILER_EMAIL,
                 pass: process.env.NODEMAILER_PASSWORD
